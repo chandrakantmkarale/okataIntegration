20febce3de8e1ba454e7a6d55caac2679928bae4
ckarale <c4crab@gmail.com>
2021-05-23 16:30:57 +0530

    first Commit

---
+++ src/main/java/com/ck/okataintegration/OkataintegrationApplication.java
@@ -0,0 +1,13 @@
+package com.ck.okataintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkataintegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OkataintegrationApplication.class, args);
+	}
+
+}
